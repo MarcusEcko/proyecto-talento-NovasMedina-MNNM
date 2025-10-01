a949e69de47bdd597af2d4fca7147a31309b2077
MarcusEcko <marco237.mnm@gmail.com>
2025-10-01 11:38:36 -0300

    spinner added

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,39 @@ import Header from "./Components/Header"
 import Hero from "./Components/Hero"
 import Sections from "./Components/Sections";
 import Featured from "./Components/Featured";
+import { Spinner } from "react-bootstrap";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [cargando, setCargando] = useState(false);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-              const resp = await fetch('https://dummyjson.com/products?limit=10&skip=10');
-              const data = await resp.json();
-              setProducts(data.products);
-            } catch (err) {
-              console.error(err);
-            }
-        }
-        fetchData();
-    }, []);
+  useEffect(() => {
+      const fetchData = async () => {
+          try {
+            const resp = await fetch('https://dummyjson.com/products?limit=10&skip=10');
+            const data = await resp.json();
+            setProducts(data.products);
+            setCargando(false);
+          } catch (err) {
+            console.error(err);
+            setError(err);
+            setCargando(false);
+          }
+      }
+      fetchData();
+  }, []);
+
+  //SPINNER
+  if(cargando) {
+    return(
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    );
+  }
+
+  if(error) return <p>{error}</p>
 
   return (
     <div>
